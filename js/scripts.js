8d704aa86aa74bad12ab89f39cd098fc6a241bf7
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-08-29 10:48:30 +0300

    adding form, styling and functionalities

--- js/scripts.js
+++ js/scripts.js
@@ -84,7 +84,7 @@ $(document).ready(function(){
   });
   
   $(document).ready(function(){
-    $("form#form34A").submit(function(event){
+    $("myForm").submit(function(event){
       
       var name = $("input#MERGE1").val();
       var email = $("input#MERGE0").val();
